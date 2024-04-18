dc43c96237efd2bcec5b2448586d4e067f31acd7
pulodomeowmeow <justin940522@gmail.com>
2024-04-19 00:10:01 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,10 +3,11 @@
 #include <stdint.h>
 #include <unistd.h>
 
-// main function
+// main function ubuntu
 int main(int argc, char const *argv[])
 {
     int32_t a = 0;
+    int32_t b = 0;
     printf("Hello, world!\n");
 
     return 0;
