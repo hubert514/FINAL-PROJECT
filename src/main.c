8e746f2c74568abe7e7aca6f4aab14236af10dd4
pulodomeowmeow <justin940522@gmail.com>
2024-04-18 23:17:57 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,11 +3,11 @@
 #include <stdint.h>
 #include <unistd.h>
 
+// main function
 int main(int argc, char const *argv[])
 {
     int32_t a = 0;
     printf("Hello, world!\n");
-    
+
     return 0;
-    
 }
