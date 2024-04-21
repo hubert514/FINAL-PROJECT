29e8f1f6c974e8605a08c3da99d587f2ee34e7f3
pulodomeowmeow <justin940522@gmail.com>
2024-04-22 01:36:27 +0800

    04/22 0136
    setting librarys

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
+#include "../include/SDL2/SDL.h"
+#include "../include/SDL2/SDL.h"
 #include "constants.h"
 #include <unistd.h>
 #include "load_image.h"
